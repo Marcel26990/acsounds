8b57d539bdf12ce50de81204fa7199a9d0fa2a8e
Marcel <41128560+Marcel26990@users.noreply.github.com>
2018-10-22 20:50:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const config = require('./config.js');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = config.prefix;
 
+bot.login(config.token);
+
+bot.on('ready', async () => {
+    console.log(`Ready as ${bot.user.tag}`);
+    setInterval(async function() {
+        let status = ['Fuck that shit I\'m outta here!', 'Hell no', `USE ${prefix}sound !!!!`, 'grab em by the pussy', 'Ey manne!', ''];
+        let chosen = status[Math.floor(Math.random() * status.length)];
+        bot.user.setActivity(chosen, {type: "PLAYING"});
+    }, 15000);
+});
+
+bot.on('message', async message => {
+    if (message.author.bot) return;
+    if (message.channel.type == "dm") return;
+
+    let args = message.content.split(" ");
+    let cont = message.content.split(" ").slice(1).join(" ");
+
+    
+});
